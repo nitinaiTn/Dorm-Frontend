b5e5e826ac63a2a9a92599ca00e63d4141541ca4
nitinaiTn <nitinai.tn@gmail.com>
2023-02-23 10:18:24 +0700

    Update DashboardAppPage.js

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -1,9 +1,35 @@
-import React, { useState, useEffect, useMomo } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { faker } from '@faker-js/faker';
+import { filter } from 'lodash';
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography, Stack } from '@mui/material';
+import { Grid,
+  Card,
+  Table,
+  Stack,
+  Paper,
+  Avatar,
+  Button,
+  Popover,
+  Checkbox,
+  TableRow,
+  MenuItem,
+  TableBody,
+  TableCell,
+  Container,
+  Typography,
+  IconButton,
+  TableContainer,
+  TablePagination,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent, 
+  DialogContentText, 
+  DialogTitle,
+  TableHead
+} from '@mui/material';
 // components
 import Iconify from '../components/iconify';
 // sections
@@ -18,23 +44,161 @@ import {
   AppCurrentSubject,
   AppConversionRates,
 } from '../sections/@dashboard/app';
+import Scrollbar from '../components/scrollbar';
+// sections
+import { UserListHead, UserListToolbar } from '../sections/@dashboard/user';
+
+// ----------------------------------------------------------------------
+
+const TABLE_HEAD = [
+  { id: 'room', label: 'ห้อง', alignRight: false },
+  { id: 'property', label: 'ตึก', alignRight: false },
+  { id: 'name', label: 'ชื่อผู้พักอาศัย', alignRight: false },
+  { id: 'status', label: 'สถานะห้อง', alignRight: false },
+  { id: 'cost', label: 'ค่าที่พักรายเดือน', alignRight: false },
+  { id: '' },
+];
+
+// ----------------------------------------------------------------------
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function applySortFilter(array, comparator, query) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  if (query) {
+    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  }
+  return stabilizedThis.map((el) => el[0]);
+}
 
 // ----------------------------------------------------------------------
 
 export default function DashboardAppPage() {
   const theme = useTheme();
-  const [dateTime, setDateTime] = useState(new Date());
 
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     setDateTime(new Date());
-  //   }, 1000);
+  const [open, setOpen] = useState(null);
 
-  //   return () => clearInterval(intervalId);
-  // }, [dateTime]);
+  const [page, setPage] = useState(0);
 
-  // const options = { timeZone: 'Asia/Bangkok' };
-  // const formattedDateTime = dateTime.toLocaleString('en-US', options);
+  const [order, setOrder] = useState('asc');
+
+  const [selected, setSelected] = useState([]);
+
+  const [orderBy, setOrderBy] = useState('name');
+
+  const [filterName, setFilterName] = useState('');
+
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const [data, setData] = useState([]);
+
+  const [utilitydata, setUtilityData] = useState([]);
+  
+  useEffect(() => {
+    async function fetchUtility() {
+      const res = await fetch("https://dorm-api.vercel.app/api/utility");
+      const data = await res.json();
+      setUtilityData(data);
+      console.log(data)
+    }
+    fetchUtility();
+
+    async function fetchData() {
+      const res = await fetch("https://dorm-api.vercel.app/api/bill");
+      const data = await res.json();
+      setData(data);
+      console.log(data)
+    }
+    fetchData();
+    
+  }, []);
+
+  const maintainLIST = data.map(item => ({
+    id: item.request_id,
+    userID: item.user_id,
+    name: item.name,
+    lastName: item.lastName,
+    status: item.request_status,
+    property: item.property_id,
+    room: item.room_id,
+  }));
+
+  const handleOpenMenu = (event) => {
+    setOpen(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setOpen(null);
+  };
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = maintainLIST.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setPage(0);
+    setRowsPerPage(parseInt(event.target.value, 10));
+  };
+
+  const handleFilterByName = (event) => {
+    setPage(0);
+    setFilterName(event.target.value);
+  };
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - maintainLIST.length) : 0;
+
+  const filteredUsers = applySortFilter(maintainLIST, getComparator(order, orderBy), filterName);
+
+  const isNotFound = !filteredUsers.length && !!filterName;
 
   return (
     <>
@@ -46,7 +210,7 @@ export default function DashboardAppPage() {
 
         <Stack direction={'row'} sx={{justifyContent: 'space-between'}}>
           <Typography variant="h4" sx={{ mb: 5 }}>
-            สวัสดี, ยินต้อนรับ
+            สวัสดี, กฤตภาส ยาโด
           </Typography>
 
           {/* <Typography variant="h4" sx={{ mb: 5 }}>
@@ -74,8 +238,8 @@ export default function DashboardAppPage() {
 
           <Grid item xs={12} md={6} lg={8}>
             <AppWebsiteVisits
-              title="อัตราการใช้หน่วยในหอพัก"
-              subheader="(+43%) than last year"
+              title="อัตราการใช้น้ำและไฟฟ้าในหอพักทั้งหมด"
+              subheader="รายการ 6 เดือนที่ผ่านมา"
               chartLabels={[
                 '01/01/2003',
                 '02/01/2003',
@@ -83,31 +247,32 @@ export default function DashboardAppPage() {
                 '04/01/2003',
                 '05/01/2003',
                 '06/01/2003',
-                '07/01/2003',
-                '08/01/2003',
-                '09/01/2003',
-                '10/01/2003',
-                '11/01/2003',
               ]}
               chartData={[
                 {
-                  name: 'Team A',
+                  name: 'ค่าน้ำ',
                   type: 'column',
                   fill: 'solid',
-                  data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
+                  data: [23, 11, 21, 44, 22, 30],
                 },
                 {
-                  name: 'Team B',
-                  type: 'area',
-                  fill: 'gradient',
-                  data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
-                },
-                {
-                  name: 'Team C',
-                  type: 'line',
+                  name: 'ค่าไฟ',
+                  type: 'column',
                   fill: 'solid',
-                  data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
+                  data: [23, 11, 22, 27, 22, 30],
                 },
+                // {
+                //   name: 'Team B',
+                //   type: 'area',
+                //   fill: 'gradient',
+                //   data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
+                // },
+                // {
+                //   name: 'Team C',
+                //   type: 'line',
+                //   fill: 'solid',
+                //   data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
+                // },
               ]}
             />
           </Grid>
@@ -130,7 +295,7 @@ export default function DashboardAppPage() {
             />
           </Grid>
 
-          <Grid item xs={12} md={6} lg={8}>
+          {/* <Grid item xs={12} md={6} lg={8}>
             <AppConversionRates
               title="Conversion Rates"
               subheader="(+43%) than last year"
@@ -147,7 +312,7 @@ export default function DashboardAppPage() {
                 { label: 'United Kingdom', value: 1380 },
               ]}
             />
-          </Grid>
+          </Grid> */}
 
           {/* <Grid item xs={12} md={6} lg={4}>
             <AppCurrentSubject
@@ -163,6 +328,120 @@ export default function DashboardAppPage() {
           </Grid> */}
 
           <Grid item xs={12} md={6} lg={8}>
+            <Card>
+              {/* <UserListToolbar numSelected={selected.length} filterName={filterName} onFilterName={handleFilterByName} /> */}
+              {/* <TableHead>
+                
+              </TableHead> */}
+              <Stack direction="row" alignItems="center" justifyContent="space-between">
+                  <Typography variant="h6" sx={{ my: 2, ml: 3 }}>
+                    สถานะห้อง
+                  </Typography>
+                  <Typography variant="h6" sx={{ my: 2, mr: 3 }}>
+                    ห้องทั้งหมด : 40 ห้องว่าง : 33
+                  </Typography>
+                 </Stack>
+
+              <Scrollbar>
+                <TableContainer sx={{ minWidth: 800 }}>
+                  <Table>
+                    <UserListHead
+                      order={order}
+                      orderBy={orderBy}
+                      headLabel={TABLE_HEAD}
+                      rowCount={maintainLIST.length}
+                      numSelected={selected.length}
+                      onRequestSort={handleRequestSort}
+                      onSelectAllClick={handleSelectAllClick}
+                    />
+                    <TableBody>
+                      {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                        const { id, requestText, userID, name, lastName, status, property, room} = row;
+                        const selectedUser = selected.indexOf(name) !== -1;
+
+                        return (
+                          <TableRow hover key={id} tabIndex={-1} role="checkbox" selected={selectedUser}>
+                            <TableCell padding="checkbox">
+                              {/* <Checkbox checked={selectedUser} onChange={(event) => handleClick(event, name)} /> */}
+                            </TableCell>
+{/* 
+                            <TableCell component="th" scope="row" padding="none">
+                              <Stack direction="row" alignItems="center" spacing={2}>
+                                <Avatar alt={name} src={avatarUrl} />
+                                <Typography variant="subtitle2" noWrap>
+                                  {requestText}
+                                </Typography>
+                              </Stack>
+                            </TableCell> */}
+
+                            <TableCell align="left">{room}</TableCell>
+
+                            <TableCell align="left">{property}</TableCell>
+
+                            <TableCell align="left">{name} {lastName}</TableCell>
+
+                            <TableCell align="left">{status}</TableCell>
+
+                            <TableCell align="left">{property}</TableCell>
+
+                            {/* <TableCell align="left">{isVerified ? 'Yes' : 'No'}</TableCell>
+
+                        <TableCell align="left">
+                          <Label color={(status === 'banned' && 'error') || 'success'}>{sentenceCase(status)}</Label>
+                        </TableCell> */}
+
+                            <TableCell align="right">
+                              <IconButton size="large" color="inherit" onClick={handleOpenMenu}>
+                                <Iconify icon={'eva:more-vertical-fill'} />
+                              </IconButton>
+                            </TableCell>
+                          </TableRow>
+                        );
+                      })}
+                      {emptyRows > 0 && (
+                        <TableRow style={{ height: 53 * emptyRows }}>
+                          <TableCell colSpan={6} />
+                        </TableRow>
+                      )}
+                    </TableBody>
+
+                    {isNotFound && (
+                      <TableBody>
+                        <TableRow>
+                          <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                            <Paper
+                              sx={{
+                                textAlign: 'center',
+                              }}
+                            >
+                              <Typography variant="h6" paragraph>
+                                ไม่มีข้อมูล
+                              </Typography>
+
+                              <Typography variant="body2">
+                                ไม่มีข้อมูล &nbsp;
+                                <strong>&quot;{filterName}&quot;</strong>.
+                                <br /> ลองใส่ข้อมูลที่ต้องการค้นหาอีกครั้ง
+                              </Typography>
+                            </Paper>
+                          </TableCell>
+                        </TableRow>
+                      </TableBody>
+                    )}
+                  </Table>
+                </TableContainer>
+              </Scrollbar>
+
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                component="div"
+                count={maintainLIST.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </Card>
             {/* <AppNewsUpdate
               title="News Update"
               list={[...Array(5)].map((_, index) => ({
@@ -193,7 +472,7 @@ export default function DashboardAppPage() {
             />
           </Grid>
 
-          <Grid item xs={12} md={6} lg={4}>
+          {/* <Grid item xs={12} md={6} lg={4}>
             <AppTrafficBySite
               title="Traffic by Site"
               list={[
@@ -232,7 +511,7 @@ export default function DashboardAppPage() {
                 { id: '5', label: 'Sprint Showcase' },
               ]}
             />
-          </Grid>
+          </Grid> */}
         </Grid>
       </Container>
     </>
